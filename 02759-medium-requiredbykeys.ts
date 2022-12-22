d0c6d4626232c6012d0e27bf4030ca20a76b1f9d
finn.xu <ouhuangff@163.com>
2022-12-22 09:47:56 +0800

    2759

---
+++ 02759-medium-requiredbykeys.ts
@@ -0,0 +1,41 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+interface User {
+  name?: string
+  age?: number
+  address?: string
+}
+
+interface UserRequiredName {
+  name: string
+  age?: number
+  address?: string
+}
+
+interface UserRequiredNameAndAge {
+  name: string
+  age: number
+  address?: string
+}
+
+type cases = [
+  Expect<Equal<RequiredByKeys<User, 'name'>, UserRequiredName>>,
+  Expect<Equal<RequiredByKeys<User, 'name' | 'age'>, UserRequiredNameAndAge>>,
+  Expect<Equal<RequiredByKeys<User>, Required<User>>>,
+  // @ts-expect-error
+  Expect<Equal<RequiredByKeys<User, 'name' | 'unknown'>, UserRequiredName>>
+]
+
+// ============= Your Code Here =============
+type FlatObj<T> = {
+  [k in keyof T]: T[k]
+}
+
+type RequiredByKeys<T, K extends keyof T = keyof T> = FlatObj<
+  {
+    [k in keyof T as k extends K ? k : never]-?: T[k]
+  } & {
+    [k in keyof T as k extends K ? never : k]: T[k]
+  }
+>

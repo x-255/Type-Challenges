efcf47e9e813505c12ad7952c09a92bcc501f5c7
finn.xu <ouhuangff@163.com>
2022-12-30 11:09:44 +0800

    3026,3088

---
+++ 03188-medium-tuple-to-nested-object.ts
@@ -0,0 +1,14 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<TupleToNestedObject<['a'], string>, { a: string }>>,
+  Expect<Equal<TupleToNestedObject<['a', 'b'], number>, { a: { b: number } }>>,
+  Expect<Equal<TupleToNestedObject<['a', 'b', 'c'], boolean>, { a: { b: { c: boolean } } }>>,
+  Expect<Equal<TupleToNestedObject<[], boolean>, boolean>>
+]
+
+// ============= Your Code Here =============
+type TupleToNestedObject<T, U> = T extends [...infer I, infer L]
+  ? TupleToNestedObject<I, { [k in L & string]: U }>
+  : U
